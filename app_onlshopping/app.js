0be9d6d97551b15be2a02d3b722ba13a78a47d65
Kaltovia <ranconbonmat@gmail.com>
2022-03-25 22:24:41 +0700

    request duoc data tu database len fontend

--- app_onlshopping/app.js
+++ app_onlshopping/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const sql = require("mssql");
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var cartRouter = require('./routes/cart');
@@ -11,63 +12,8 @@ var signupRouter = require('./routes/signup')
 var loginRouter = require('./routes/login');
 var productRouter = require('./routes/product')
 
-
 var app = express();
-var Connection = require('tedious').Connection;  
-    var config = {  
-        server: 'localhost',  //update me
-        authentication: {
-            type: 'default',
-            options: {
-                userName: 'sa', //update me
-                password: '123456'  //update me
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'qlbh_onl'  //update me
-        }
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
-        console.log("Connected");  
-    });
-    
-    connection.connect();
-    var Request = require('tedious').Request;  
-    var TYPES = require('tedious').TYPES;  
-  
-    function executeStatement() {  
-        request = new Request("SELECT * FROM ACCOUNT;", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-        });
-        connection.execSql(request);  
-    }  
-    executeStatement();
+
 // view engine setupS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -82,7 +28,7 @@ app.use('/', indexRouter);
 app.use('/cart', cartRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
-app.use('/product', productRouter);
+app.use('/product', productRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
